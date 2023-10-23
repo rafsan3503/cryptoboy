baace94062f54f767093860c03dffea529830b32
Md Mahmud Hasan <108387443+rafsan3503@users.noreply.github.com>
2023-10-23 15:19:52 +0600

    first commit

---
+++ js/commands.js
@@ -0,0 +1,116 @@
+
+var telegram = "tglink";
+var twitter = "twitterlink";
+// var instagram = "https://www.instagram.com/_i_am_pratyush04/";
+// var github = "https://github.com/pratyushsingha";
+// var hackerrank = "https://www.hackerrank.com/pratyushsingha83";
+// var sudo = "https://www.youtube.com/embed/tgbNymZ7vqY";
+var email = "mailto:contact@cryptoboy.bio";
+
+aboutus = [
+  "<br>",
+  "<span class='underline'>Welcome To Crypto Boy!!👋</span>",
+  "<br>",
+
+  "👨‍💻 $CBOY is a place to celebrate anonymity at it's finest.👨‍💻",
+  "<br>",
+
+  `<pre class="whitespace-pre-wrap">
+  Our project will focus on sharing & highlighting 
+  all pertinent information to our members 
+  who can trade anonymously without having to be concerned or
+  influenced by the words or actions of others.`,
+  "<br>",
+  `<pre class="whitespace-pre-wrap">
+  In staying true to the project's core value in being anonymous, 
+  it's up to every individual to decide on their own if they choose to join 
+  and/or create community TG's of their own.
+
+  "You will not see us or hear us. We are all Pepe Anons"`,
+  
+  
+  // `<li>📫 Contact us at <a href= "mailto:contact@cryptoboy.bio" target="_blank">contact@cryptoboy.bio</a>`,
+  // "<br>",
+];
+
+social = [
+  "<br>",
+  'Telegram        <a href="' +
+  telegram +
+  '" target="blank">telegram/crypto_boy' +
+  "</a>",
+  'Twitter     <a href="' +
+  twitter +
+  '" target="blank">twitter/crypto_boy' +
+  "</a>",
+  // 'Instagram  <a href="' +
+  // instagram +
+  // '" target="blank">instagram/pratyushsingha' +
+  // "</a>",
+  // 'Github         <a href="' +
+  // github +
+  // '" target="blank">github/pratyushsingha' +
+  // "</a>",
+  // 'Hackerrank  <a href="' +
+  // hackerrank +
+  // '" target="blank">hackerrank/pratyushsingha' +
+  // "</a>",
+
+  "<br>",
+];
+
+tokenomics =[
+  "<br>",
+  `<span class='underline'>Token Supply</span>
+  - 1 Million (1,000,000)</pre>`,
+  `<span class='underline'>Total Tax</span>
+  - 2/2 On Buys & Sells</pre>`,
+  `<span class='underline'>Token CA</span>
+  0x0000000000000000000</pre>`,
+  // `<pre class="whitespace-pre-wrap skills">
+  // - Tailwind CSS</pre>`,
+  // `<pre class="whitespace-pre-wrap skills">
+  // - React</pre>`,
+  // `<pre class="whitespace-pre-wrap skills">
+  // - Git(Versions Control)</pre>`,
+  // `<pre class="whitespace-pre-wrap skills">
+  // - Linux</pre>`,
+  // `<pre class="whitespace-pre-wrap skills">
+  // - Web Hosting(Azure, Vercel, Netlify,Digital Ocean)</pre>`,
+  "<br>"
+   
+];
+
+
+projects = [
+  "<br>",
+  `<a href="https://dapp.pepeanon.site" target="blank"><span class='underline'>Future Utility</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap"> Will have practical applications beyond its value as just a token
+
+`,
+  "<br>",
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutus</span>    
+   <span class="command">↳</span>&nbsp;Diplays What Is Pepe Anons XAI?
+  <span class="command">tokenomics</span>     
+   <span class="command">↳</span>&nbsp;Lists token distribution.
+  <span class="command">socials</span>     
+   <span class="command">↳</span>&nbsp;List of social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;Will list future utility.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;Contact for inquiries.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome To Pepe Anons XAI — Type <span class="command text-[#75e1e7]">help</span> For A List Of Supported Commands.</span></div>`,
+];
